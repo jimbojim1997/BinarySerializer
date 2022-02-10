90e11900b87d5159ee66dbafef7457eff0404b20
James Duda <jimbojim1997@gmail.com>
2022-02-10 00:22:37 +0000

    BinarySerializer: Added SerializationHelpers with tests.

---
+++ BinarySerializer/SerializationHelpers.cs
@@ -0,0 +1,180 @@
+﻿using System.IO;
+
+namespace BinarySerializer
+{
+    internal static class SerializationHelpers
+    {
+        internal static void SerializeByte(byte value, Stream stream)
+        {
+            stream.WriteByte(value);
+        }
+
+        internal static byte DeserializeByte(Stream stream)
+        {
+            return (byte)stream.ReadByte();
+        }
+
+        internal static void SerializeSByte(sbyte value, Stream stream)
+        {
+            SerializeByte((byte)value, stream);
+        }
+
+        internal static sbyte DeserializeSByte(Stream stream)
+        {
+            return (sbyte)DeserializeByte(stream);
+        }
+
+        internal static void SerializeBoolean(bool value, Stream stream)
+        {
+            stream.WriteByte((byte)(value ? 1 : 0));
+        }
+
+        internal static bool DeserializeBoolean(Stream stream)
+        {
+            return stream.ReadByte() != 0;
+        }
+
+        internal static void SerializeShort(short value, Stream stream)
+        {
+            byte[] bytes = new byte[] {
+                (byte)(value),
+                (byte)(value >> 8)
+            };
+            stream.Write(bytes, 0, bytes.Length);
+        }
+
+        internal static short DeserializeShort(Stream stream)
+        {
+            byte[] bytes = new byte[2];
+            stream.Read(bytes, 0, bytes.Length);
+            return (short)(bytes[0] | bytes[1] << 8);
+        }
+
+        internal static void SerializeUShort(ushort value, Stream stream)
+        {
+            SerializeShort((short)value, stream);
+        }
+
+        internal static ushort DeserializeUShort(Stream stream)
+        {
+            return (ushort)DeserializeShort(stream);
+        }
+
+        internal static void SerializeChar(char value, Stream stream)
+        {
+            SerializeShort((short)value, stream);
+        }
+
+        internal static char DeserializeChar(Stream stream)
+        {
+            return (char)DeserializeShort(stream);
+        }
+
+        internal static void SerializeInt(int value, Stream stream)
+        {
+            byte[] bytes = new byte[] {
+                (byte)(value),
+                (byte)(value >> 8),
+                (byte)(value >> 16),
+                (byte)(value >> 24)
+            };
+            stream.Write(bytes, 0, bytes.Length);
+        }
+
+        internal static int DeserializeInt(Stream stream)
+        {
+            byte[] bytes = new byte[4];
+            stream.Read(bytes, 0, bytes.Length);
+            return (int)(bytes[0] | bytes[1] << 8 | bytes[2] << 16 | bytes[3] << 24);
+        }
+
+        internal static void SerializeUInt(uint value, Stream stream)
+        {
+            SerializeInt((int)value, stream);
+        }
+
+        internal static uint DeserializeUInt(Stream stream)
+        {
+            return (uint)DeserializeInt(stream);
+        }
+
+        internal static void SerializeFloat(float value, Stream stream)
+        {
+            unsafe
+            {
+                SerializeInt(*(int*)&value, stream);
+            }
+        }
+
+        internal static float DeserializeFloat(Stream stream)
+        {
+            unsafe
+            {
+                int value = DeserializeInt(stream);
+                return *(float*)&value;
+            }
+        }
+
+        internal static void SerializeLong(long value, Stream stream)
+        {
+            byte[] bytes = new byte[] {
+                (byte)(value),
+                (byte)(value >> 8),
+                (byte)(value >> 16),
+                (byte)(value >> 24),
+                (byte)(value >> 32),
+                (byte)(value >> 40),
+                (byte)(value >> 48),
+                (byte)(value >> 56)
+            };
+            stream.Write(bytes, 0, bytes.Length);
+        }
+
+        internal static long DeserializeLong(Stream stream)
+        {
+            byte[] bytes = new byte[8];
+            stream.Read(bytes, 0, bytes.Length);
+            long low = ((long)bytes[0] | (long)bytes[1] << 8 | (long)bytes[2] << 16 | (long)bytes[3] << 24);
+            long high = ((long)bytes[4] | (long)bytes[5] << 8 | (long)bytes[6] << 16 | (long)bytes[7] << 24);
+            return low | high << 32;
+        }
+
+        internal static void SerializeULong(ulong value, Stream stream)
+        {
+            SerializeLong((long)value, stream);
+        }
+
+        internal static ulong DeserializeULong(Stream stream)
+        {
+            return (ulong)DeserializeLong(stream);
+        }
+
+        internal static void SerializeDouble(double value, Stream stream)
+        {
+            unsafe
+            {
+                SerializeLong(*(long*)&value, stream);
+            }
+        }
+
+        internal static double DeserializeDouble(Stream stream)
+        {
+            unsafe
+            {
+                long value = DeserializeLong(stream);
+                return *(double*)&value;
+            }
+        }
+
+        internal static void SerializeObjectId(uint objectId, Stream stream)
+        {
+            SerializeUInt(objectId, stream);
+        }
+
+        internal static uint DeserializeObjectId(Stream stream)
+        {
+            return DeserializeUInt(stream);
+        }
+
+    }
+}
---
+++ Tests/SerializationHelpers/SerializationHelpersRoundTrip.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.IO;
+
+using BinarySerializer;
+
+namespace Tests.SerializationHelpersTests
+{
+    [TestClass]
+    public class SerializationHelpersRoundTrip
+    {
+        [DataTestMethod]
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(254)]
+        [DataRow(255)]
+        public void ByteRoundTrip(int value)
+        {
+            AssertRoundTrip((byte)value, SerializationHelpers.SerializeByte, SerializationHelpers.DeserializeByte);
+        }
+
+        [DataTestMethod]
+        [DataRow(-128)]
+        [DataRow(-127)]
+        [DataRow(-1)]
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(126)]
+        [DataRow(127)]
+        public void SByteRoundTrip(int value)
+        {
+            AssertRoundTrip((sbyte)value, SerializationHelpers.SerializeSByte, SerializationHelpers.DeserializeSByte);
+        }
+
+        [DataTestMethod]
+        [DataRow(false)]
+        [DataRow(true)]
+        public void BooleanRoundTrip(bool value)
+        {
+            AssertRoundTrip(value, SerializationHelpers.SerializeBoolean, SerializationHelpers.DeserializeBoolean);
+        }
+
+        [DataTestMethod]
+        [DataRow(-32768)]
+        [DataRow(-32767)]
+        [DataRow(-1)]
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(32766)]
+        [DataRow(32767)]
+        public void ShortRoundTrip(int value)
+        {
+            AssertRoundTrip((short)value, SerializationHelpers.SerializeShort, SerializationHelpers.DeserializeShort);
+        }
+
+        [DataTestMethod]
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(65534)]
+        [DataRow(65535)]
+        public void UShortRoundTrip(int value)
+        {
+            AssertRoundTrip((ushort)value, SerializationHelpers.SerializeUShort, SerializationHelpers.DeserializeUShort);
+        }
+
+        [DataTestMethod]
+        [DataRow((char)0)]
+        [DataRow((char)1)]
+        [DataRow('a')]
+        [DataRow('Z')]
+        [DataRow((char)65534)]
+        [DataRow((char)65535)]
+        public void CharRoundTrip(char value)
+        {
+            AssertRoundTrip(value, SerializationHelpers.SerializeChar, SerializationHelpers.DeserializeChar);
+        }
+
+        [DataTestMethod]
+        [DataRow(-2147483648)]
+        [DataRow(-2147483647)]
+        [DataRow(-1)]
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(2147483646)]
+        [DataRow(2147483647)]
+        public void IntRoundTrip(int value)
+        {
+            AssertRoundTrip(value, SerializationHelpers.SerializeInt, SerializationHelpers.DeserializeInt);
+        }
+
+        [DataTestMethod]
+        [DataRow(0U)]
+        [DataRow(1U)]
+        [DataRow(4294967294U)]
+        [DataRow(4294967295U)]
+        public void UIntRoundTrip(uint value)
+        {
+            AssertRoundTrip(value, SerializationHelpers.SerializeUInt, SerializationHelpers.DeserializeUInt);
+        }
+
+        [DataTestMethod]
+        [DataRow(-2147483648f)]
+        [DataRow(-2147483647f)]
+        [DataRow(-12.345f)]
+        [DataRow(-1f)]
+        [DataRow(0f)]
+        [DataRow(1f)]
+        [DataRow(12.345f)]
+        [DataRow(2147483646f)]
+        [DataRow(2147483647f)]
+        public void FloatRoundTrip(float value)
+        {
+            AssertRoundTrip(value, SerializationHelpers.SerializeFloat, SerializationHelpers.DeserializeFloat);
+        }
+
+        [DataTestMethod]
+        [DataRow(-9223372036854775808L)]
+        [DataRow(-9223372036854775807L)]
+        [DataRow(-1L)]
+        [DataRow(0L)]
+        [DataRow(1L)]
+        [DataRow(9223372036854775806L)]
+        [DataRow(9223372036854775807L)]
+        public void LongRoundTrip(long value)
+        {
+            AssertRoundTrip(value, SerializationHelpers.SerializeLong, SerializationHelpers.DeserializeLong);
+        }
+
+        [DataTestMethod]
+        [DataRow(0UL)]
+        [DataRow(1UL)]
+        [DataRow(18446744073709551614UL)]
+        [DataRow(18446744073709551615UL)]
+        public void ULongRoundTrip(ulong value)
+        {
+            AssertRoundTrip(value, SerializationHelpers.SerializeULong, SerializationHelpers.DeserializeULong);
+        }
+
+        [DataTestMethod]
+        [DataRow(-9223372036854775808)]
+        [DataRow(-9223372036854775807D)]
+        [DataRow(-12.345D)]
+        [DataRow(-1D)]
+        [DataRow(0D)]
+        [DataRow(1D)]
+        [DataRow(12.345D)]
+        [DataRow(9223372036854775806D)]
+        [DataRow(9223372036854775807D)]
+        public void DoubleRoundTrip(double value)
+        {
+            AssertRoundTrip(value, SerializationHelpers.SerializeDouble, SerializationHelpers.DeserializeDouble);
+        }
+
+        private void AssertRoundTrip<T>(T expected, Action<T, Stream> serialize, Func<Stream, T> deserialize)
+        {
+            using(MemoryStream ms = new MemoryStream())
+            {
+                serialize(expected, ms);
+                ms.Seek(0, SeekOrigin.Begin);
+                T actual = deserialize(ms);
+
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
