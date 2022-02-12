043003f5c69aaded2cab691ccabf94f64390f04a
James Duda <jimbojim1997@gmail.com>
2022-02-12 00:20:07 +0000

    ReflectionBinarySerializer: Implemented deserialization.

---
+++ BinarySerializer/DeserializedObjectsCollection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace BinarySerializer
+{
+    internal class DeserializedObjectsCollection
+    {
+        private const uint NULL_OBJECT_ID = 0;
+        private Dictionary<uint, object> _objects = new Dictionary<uint, object>()
+        {
+            {NULL_OBJECT_ID, null}
+        };
+
+
+        public void Add(uint objectId, object obj)
+        {
+            _objects.Add(objectId, obj);
+        }
+
+        public bool TryGetObject(uint objectId, out object obj)
+        {
+            return _objects.TryGetValue(objectId, out obj);
+        }
+    }
+}
--- BinarySerializer/ReflectionBinarySerializer.cs
+++ BinarySerializer/ReflectionBinarySerializer.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace BinarySerializer
 {
     public class ReflectionBinarySerializer : IBinarySerializer
     {
-        private static readonly MethodInfo _serialize = typeof(ReflectionBinarySerializer).GetMethod("Serialize", BindingFlags.NonPublic | BindingFlags.Instance);
+        private static readonly MethodInfo _serialize = typeof(ReflectionBinarySerializer).GetMethod(nameof(Serialize), BindingFlags.NonPublic | BindingFlags.Instance);
+        private static readonly MethodInfo _deserialize = typeof(ReflectionBinarySerializer).GetMethod(nameof(Deserialize), BindingFlags.NonPublic | BindingFlags.Instance);
 
         public void Serialize<T>(T obj, Stream stream)
         {
@@ -38,7 +40,7 @@ namespace BinarySerializer
                 else if (obj is decimal de) SerializationHelpers.SerializeDecimal(de, stream);
                 else throw new Exception($"Primitive type \"{type.FullName}\" not supported.");
             }
-            else if(obj is string str)
+            else if (obj is string str)
             {
                 uint objectId;
                 if (serializedObjects.TryGetObjectId((object)obj, out objectId))
@@ -89,8 +91,9 @@ namespace BinarySerializer
                 {
                     objectId = serializedObjects.Add((object)obj);
                     SerializationHelpers.SerializeObjectId(objectId, stream);
-                    foreach (FieldInfo field in typeof(T).GetRuntimeFields())
+                    foreach (FieldInfo field in type.GetRuntimeFields())
                     {
+                        if (field.IsLiteral || field.IsStatic) continue;
                         object value = field.GetValue(obj);
                         MethodInfo valueSerialize = _serialize.MakeGenericMethod(field.FieldType);
                         valueSerialize.Invoke(this, new[] { value, stream, serializedObjects });
@@ -99,8 +102,9 @@ namespace BinarySerializer
             }
             else if (type.IsValueType)
             {
-                foreach (FieldInfo field in typeof(T).GetRuntimeFields())
+                foreach (FieldInfo field in type.GetRuntimeFields())
                 {
+                    if (field.IsLiteral || field.IsStatic) continue;
                     object value = field.GetValue(obj);
                     MethodInfo valueSerialize = _serialize.MakeGenericMethod(field.FieldType);
                     valueSerialize.Invoke(this, new[] { value, stream, serializedObjects });
@@ -114,7 +118,111 @@ namespace BinarySerializer
 
         public T Deserialize<T>(Stream stream)
         {
-            throw new NotImplementedException();
+            return Deserialize<T>(stream, new DeserializedObjectsCollection());
+        }
+
+        private T Deserialize<T>(Stream stream, DeserializedObjectsCollection deserializedObjects)
+        {
+            Type type = typeof(T);
+
+            if (type.IsPrimitive)
+            {
+                object obj;
+                if (type.Equals(typeof(byte))) obj = SerializationHelpers.DeserializeByte(stream);
+                else if (type.Equals(typeof(sbyte))) obj = SerializationHelpers.DeserializeSByte(stream);
+                else if (type.Equals(typeof(bool))) obj = SerializationHelpers.DeserializeBoolean(stream);
+                else if (type.Equals(typeof(short))) obj = SerializationHelpers.DeserializeShort(stream);
+                else if (type.Equals(typeof(ushort))) obj = SerializationHelpers.DeserializeUShort(stream);
+                else if (type.Equals(typeof(char))) obj = SerializationHelpers.DeserializeChar(stream);
+                else if (type.Equals(typeof(int))) obj = SerializationHelpers.DeserializeInt(stream);
+                else if (type.Equals(typeof(uint))) obj = SerializationHelpers.DeserializeUInt(stream);
+                else if (type.Equals(typeof(float))) obj = SerializationHelpers.DeserializeFloat(stream);
+                else if (type.Equals(typeof(long))) obj = SerializationHelpers.DeserializeLong(stream);
+                else if (type.Equals(typeof(ulong))) obj = SerializationHelpers.DeserializeULong(stream);
+                else if (type.Equals(typeof(double))) obj = SerializationHelpers.DeserializeDouble(stream);
+                else if (type.Equals(typeof(decimal))) obj = SerializationHelpers.DeserializeDecimal(stream);
+                else throw new Exception($"Primitive type \"{type.FullName}\" not supported.");
+                return (T)obj;
+            }
+            else if (type.Equals(typeof(string)))
+            {
+                uint objectId = SerializationHelpers.DeserializeObjectId(stream);
+                if (deserializedObjects.TryGetObject(objectId, out object obj))
+                {
+                    return (T)obj;
+                }
+                else
+                {
+                    uint length = SerializationHelpers.DeserializeUInt(stream);
+                    byte[] bytes = new byte[(int)length];
+                    stream.Read(bytes, 0, bytes.Length);
+                    obj = Encoding.UTF8.GetString(bytes);
+                    deserializedObjects.Add(objectId, obj);
+                    return (T)obj;
+                }
+            }
+            else if (type.IsArray)
+            {
+                uint objectId = SerializationHelpers.DeserializeObjectId(stream);
+                if (deserializedObjects.TryGetObject(objectId, out object obj))
+                {
+                    return (T)obj;
+                }
+                else
+                {
+                    uint length = SerializationHelpers.DeserializeUInt(stream);
+                    Array array = Array.CreateInstance(type.GetElementType(), (int)length);
+                    deserializedObjects.Add(objectId, array);
+
+                    MethodInfo elementDeserialize = _deserialize.MakeGenericMethod(type.GetElementType());
+
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        obj = elementDeserialize.Invoke(this, new object[] { stream, deserializedObjects });
+                        array.SetValue(obj, i);
+                    }
+                    return (T)(object)array;
+                }
+            }
+            else if (type.IsClass)
+            {
+                uint objectId = SerializationHelpers.DeserializeObjectId(stream);
+                if (deserializedObjects.TryGetObject(objectId, out object obj))
+                {
+                    return (T)obj;
+                }
+                else
+                {
+                    obj = FormatterServices.GetUninitializedObject(type);
+                    deserializedObjects.Add(objectId, obj);
+
+                    foreach (FieldInfo field in type.GetRuntimeFields())
+                    {
+                        if (field.IsLiteral || field.IsStatic) continue;
+                        MethodInfo valueDeserialize = _deserialize.MakeGenericMethod(field.FieldType);
+                        object value = valueDeserialize.Invoke(this, new object[] { stream, deserializedObjects });
+                        field.SetValue(obj, value);
+                    }
+                    return (T)obj;
+                }
+            }
+            else if (type.IsValueType)
+            {
+                object obj = FormatterServices.GetUninitializedObject(type);
+
+                foreach (FieldInfo field in type.GetRuntimeFields())
+                {
+                    if (field.IsLiteral || field.IsStatic) continue;
+                    MethodInfo valueDeserialize = _deserialize.MakeGenericMethod(field.FieldType);
+                    object value = valueDeserialize.Invoke(this, new object[] { stream, deserializedObjects });
+                    field.SetValue(obj, value);
+                }
+                return (T)obj;
+            }
+            else
+            {
+                throw new Exception($"Type \"{type.FullName}\" not supported.");
+            }
         }
     }
 }
