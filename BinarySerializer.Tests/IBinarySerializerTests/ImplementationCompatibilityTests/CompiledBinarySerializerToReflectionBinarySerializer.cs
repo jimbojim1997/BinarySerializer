5e064f834a7c6f5e03daffaef249a18c7bf35ffd
James Duda <jimbojim1997@gmail.com>
2022-02-17 21:17:28 +0000

    BinarySerializer.Tests: Added ImplementationCompatibilityTests.

---
+++ BinarySerializer.Tests/IBinarySerializerTests/ImplementationCompatibilityTests/CompiledBinarySerializerToReflectionBinarySerializer.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.IO;
+
+namespace BinarySerializer.Tests.IBinarySerializerTests.ImplementationCompatibilityTests
+{
+    [TestClass]
+    public class CompiledBinarySerializerToReflectionBinarySerializer : BinarySerializerRountTripTestsBase
+    {
+        protected override void AssertRoundTrip<T>(T expected)
+        {
+            using(MemoryStream ms = new MemoryStream())
+            {
+                var compiledBinarySerializer = new CompiledBinarySerializer();
+                var reflectionBinarySerializer = new ReflectionBinarySerializer();
+
+                compiledBinarySerializer.Serialize(expected, ms);
+                ms.Seek(0, SeekOrigin.Begin);
+                T actual = reflectionBinarySerializer.Deserialize<T>(ms);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+        protected override void AssertRoundTrip<T>(T expected, Func<T, T, bool> areEqual)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                var compiledBinarySerializer = new CompiledBinarySerializer();
+                var reflectionBinarySerializer = new ReflectionBinarySerializer();
+
+                compiledBinarySerializer.Serialize(expected, ms);
+                ms.Seek(0, SeekOrigin.Begin);
+                T actual = reflectionBinarySerializer.Deserialize<T>(ms);
+
+                Assert.IsTrue(areEqual(expected, actual));
+            }
+        }
+    }
+}
---
+++ BinarySerializer.Tests/IBinarySerializerTests/ImplementationCompatibilityTests/ReflectionBinarySerializerToCompiledBinarySerializer.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.IO;
+
+namespace BinarySerializer.Tests.IBinarySerializerTests.ImplementationCompatibilityTests
+{
+    [TestClass]
+    public class ReflectionBinarySerializerToCompiledBinarySerializer : BinarySerializerRountTripTestsBase
+    {
+        protected override void AssertRoundTrip<T>(T expected)
+        {
+            using(MemoryStream ms = new MemoryStream())
+            {
+                var compiledBinarySerializer = new CompiledBinarySerializer();
+                var reflectionBinarySerializer = new ReflectionBinarySerializer();
+
+                reflectionBinarySerializer.Serialize(expected, ms);
+                ms.Seek(0, SeekOrigin.Begin);
+                T actual = compiledBinarySerializer.Deserialize<T>(ms);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+        protected override void AssertRoundTrip<T>(T expected, Func<T, T, bool> areEqual)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                var compiledBinarySerializer = new CompiledBinarySerializer();
+                var reflectionBinarySerializer = new ReflectionBinarySerializer();
+
+                reflectionBinarySerializer.Serialize(expected, ms);
+                ms.Seek(0, SeekOrigin.Begin);
+                T actual = compiledBinarySerializer.Deserialize<T>(ms);
+
+                Assert.IsTrue(areEqual(expected, actual));
+            }
+        }
+    }
+}
